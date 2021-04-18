2514ac5d50e6f80b66e3c2936e418a9ff989e5a2
diego <dialgop@gmail.com>
2021-04-19 00:32:37 +0200

    Replacement of repetitive code with the method continueMenu().

--- src/main/Main.java
+++ src/main/Main.java
@@ -8,14 +8,15 @@ public class Main {
     /**
      * @author Diego Alejandro Gómez Pardo.
      */
+
+    /**
+     * Hauptmenü: Ein Menü zur Auswahl der 3 Optionen, die sich auf die Aufgabe beziehen, wird eingesetzt:
+     * 1. Hinzufügen von Fahrzeugen zur DB aus einer XML-Datei.
+     * 2. Hinzufügen von Kunden zur DB oder Anzeigen von Kunden aus der DB
+     * 3. Erstellen von Briefen auf der Basis von Kunden und Fahrzeugen.
+     * Nachdem eine Option ausgeführt wurde, erscheint wieder das Hauptmenü
+     */
     public static void main(String[] args) throws IOException {
-        /**
-         * Hauptmenü: Ein Menü zur Auswahl der 3 Optionen, die sich auf die Aufgabe beziehen, wird eingesetzt:
-         * 1. Hinzufügen von Fahrzeugen zur DB aus einer XML-Datei.
-         * 2. Hinzufügen von Kunden zur DB oder Anzeigen von Kunden aus der DB
-         * 3. Erstellen von Briefen auf der Basis von Kunden und Fahrzeugen.
-         * Nachdem eine Option ausgeführt wurde, erscheint wieder das Hauptmenü
-         */
 
         Scanner input = new Scanner(System.in);
         while(true) {
@@ -32,22 +33,19 @@ public class Main {
                     System.out.println("Bitte geben Sie den Pfad der xml-Datei an");
                     String path = input.nextLine();
                     readData.saveVehiclesinDB(path);//"/home/diego/Downloads/autos.xml"
-                    System.out.println("\nDrücken Sie eine beliebige Taste, um zurück zum Hauptmenü zu gelangen\n");
-                    input.nextLine();
+                    continueMenu();
                     break;
                 case "K":
                 case "k":
                     ClientControl.startClientControl();
-                    System.out.println("\nDrücken Sie eine beliebige Taste, um zurück zum Hauptmenü zu gelangen\n");
-                    input.nextLine();
+                    continueMenu();
                     break;
                 case "D":
                 case "d":
                     List<Client> clientList = ClientControl.loadClientsFromDB();
                     List<Vehicle> vehicleList = VehicleControl.loadVehiclesFromDB();
                     PDFPrinter.generatePDF(clientList,vehicleList);
-                    System.out.println("\nDrücken Sie eine beliebige Taste, um zurück zum Hauptmenü zu gelangen\n");
-                    input.nextLine();
+                    continueMenu();
                     break;
                 case "Q":
                 case "q":
@@ -56,6 +54,16 @@ public class Main {
                     break;
             }
         }
+    }
+
+    /**
+     * Methode zum Anhalten des Menüs, damit der Benutzer Änderungen an den Daten sehen kann.
+     */
+    private static void continueMenu(){
+        Scanner option = new Scanner(System.in);
+        System.out.println("\nDrücken Sie eine beliebige Taste, um zurück zum Hauptmenü zu gelangen\n");
+        option.nextLine();
+        return;
     }
 }
 
