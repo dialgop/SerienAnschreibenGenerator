691beeb6eb9ca419c913170c47a795bbdfdc0f98
diego <dialgop@gmail.com>
2021-04-19 00:44:30 +0200

    Addition of folders with JavaDoc, Class diagram from plantuml, and out folder

---
+++ javadoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"main","l":"Client"},{"p":"main","l":"ClientControl"},{"p":"main","l":"Main"},{"p":"main","l":"PDFPrinter"},{"p":"main","l":"Validation"},{"p":"main","l":"Vehicle"},{"p":"main","l":"VehicleControl"}]
